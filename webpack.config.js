9934d3db506d9c718f78d42fc91747815e3dd55e
kigpand <kigpand@naver.com>
2022-07-17 19:55:40 +0900

    webpack 주요속성 공부 및 정리

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ var path = require('path');
 
 module.exports = {
     mode: 'none',
-    entry: './src/index.js',
+    entry: {
+        local: './src/index.js',
+        main: './src/test.js'
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
